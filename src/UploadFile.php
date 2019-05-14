d3ab2f1f7fc6b7e66522f94069d12622aacb4d13
ly-ali <110>
2019-05-14 21:11:01 +0800

    test

---
+++ src/UploadFile.php
@@ -0,0 +1,8 @@
+<?php
+namespace renminlu\phpupload;
+
+class UploadFile{
+    public function begin(){
+        echo "begin upload......";
+    }
+}
---
+++ tests/index.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use renminlu\phpupload\UploadFile;
+
+$a = new UploadFile();
+$a->begin();
